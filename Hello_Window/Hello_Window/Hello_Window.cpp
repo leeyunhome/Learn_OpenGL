a269ec5d64741f8d0b803b4dd2cd90a985d9279b
leeyunhome <leeyunhome@daum.net>
2020-07-18 21:20:25 +0900

    revert to triangle

--- Hello_Window/Hello_Window/Hello_Window.cpp
+++ Hello_Window/Hello_Window/Hello_Window.cpp
@@ -119,24 +119,24 @@ int main()
 
 	// set up vertex data (and buffer(s)) and configure vertex attributes
 	// ------------------------------------------------------------------
-	//float vertices[] = {
-	//-0.5f, -0.5f, 0.0f, // left
-	// 0.5f, -0.5f, 0.0f, // right
-	// 0.0f,  0.5f, 0.0f, // top
-	//};
-
 	float vertices[] = {
-		 0.5f,  0.5f, 0.0f, // top right
-		 0.5f, -0.5f, 0.0f, // bottom right
-		-0.5f, -0.5f, 0.0f, // bottom left
-		-0.5f,  0.5f, 0.0f	// top left
+	-0.5f, -0.5f, 0.0f, // left
+	 0.5f, -0.5f, 0.0f, // right
+	 0.0f,  0.5f, 0.0f, // top
 	};
-	unsigned int indices[] = {
-		0, 1, 3,	// first triangle
-		1, 2, 3		// second triangle
-	};
-	unsigned int EBO;
-	glGenBuffers(1, &EBO);
+
+	//float vertices[] = {
+	//	 0.5f,  0.5f, 0.0f, // top right
+	//	 0.5f, -0.5f, 0.0f, // bottom right
+	//	-0.5f, -0.5f, 0.0f, // bottom left
+	//	-0.5f,  0.5f, 0.0f	// top left
+	//};
+	//unsigned int indices[] = {
+	//	0, 1, 3,	// first triangle
+	//	1, 2, 3		// second triangle
+	//};
+	//unsigned int EBO;
+	//glGenBuffers(1, &EBO);
 
 	unsigned int VBO;
 	glGenBuffers(1, &VBO);
@@ -152,8 +152,8 @@ int main()
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	// 3. copy our index array in a element buffer for OpenGL to use
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	// 1. then set the vertex attributes pointers
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
@@ -167,7 +167,7 @@ int main()
 	glBindVertexArray(0);
 
 	// uncomment this call to draw in ireframe polygons.
-	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 	// render loop
 	// -----------
@@ -185,8 +185,8 @@ int main()
 		// 2. use our shader program when we want to render an object
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		//glDrawArrays(GL_TRIANGLES, 0, 3);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		//glBindVertexArray(0); // no need to unbind it every time
 
